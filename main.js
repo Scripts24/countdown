90a624f08743c1e2856ba1e0758b6be41ec98bbf
Patricia Atanasoff <system.informatica365@gmail.com>
2025-05-15 16:37:04 -0300

    countdown reset, star effects

--- main.js
+++ main.js
@@ -8,14 +8,13 @@ let hh = document.getElementById('hh')
 let mm = document.getElementById('mm')
 let ss = document.getElementById('ss')
 
-
 // Formato fecha mes-dia-año
-let endDate = '01/01/2025 00:00:00'
+let endDate = new Date(new Date().getFullYear() + 1, 0, 1, 0, 0, 0);
 
 let x = setInterval(function () {
-    let now = new Date(endDate).getTime()
-    let contDown = new Date().getTime()
-    let distance = now - contDown
+    let now = new Date().getTime()
+    let contDown = endDate.getTime()
+    let distance = contDown - now
 
     // Cálculo de tiempo por días, horas, minutos y segundos
 
@@ -33,15 +32,48 @@ let x = setInterval(function () {
 
     dd.style.strokeDashoffset = 440 - (440 * d) / 365
     hh.style.strokeDashoffset = 440 - (440 * h) / 24
-    mm.style.strokeDashoffset = 440 - (440 * m) / 60
-    ss.style.strokeDashoffset = 440 - (440 * s) / 60
 
-    // Si la cuenta ha terminado se muestra el texto "Feliz año nuevo"
+    // Simula que la cuenta regresiva ha llegado al final
+    //endDate = new Date(new Date().getFullYear(), 0, 1, 0, 0, 0);
 
+    // Si la cuenta ha terminado, reinicia la cuenta regresiva para el próximo año
     if (distance < 0) {
-        clearInterval(x)
+        endDate = new Date(new Date().getFullYear() + 1, 0, 1, 0, 0, 0);
+        clearInterval(x);
         document.getElementById('time').style.display = "none"
-        document.querySelector(".newYear").style.display = "block"
+        document.getElementById('newYearMessage').style.display = "block"
+        document.getElementById('newYearMessage').innerHTML = (new Date().getFullYear() + 1) + "<br><span>Feliz Año Nuevo 🥂!!!</span>"; // Actualiza el mensaje de Año Nuevo
+        document.querySelector('.fireworks').classList.remove('hidden');
+        setTimeout(function () {
+            document.getElementById('time').style.display = "block"
+            document.getElementById('newYearMessage').style.display = "none"
+            document.querySelector('.fireworks').classList.add('hidden');
+            x = setInterval(function () {
+                // ...
+            }, 1000);
+        }, 5000); // tiempo de espera para mostrar el texto "Feliz año nuevo"
     }
+}, 1000);
 
-});
+const fireworks = document.querySelector('.fireworks');
+
+function createSpark(x, y) {
+    const spark = document.createElement('div');
+    spark.classList.add('spark');
+    spark.style.top = `${y}px`;
+    spark.style.left = `${x}px`;
+    fireworks.appendChild(spark);
+}
+
+function animateFireworks() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    for (let i = 0; i < 100; i++) {
+        const x = Math.random() * width;
+        const y = Math.random() * height;
+        createSpark(x, y);
+    }
+    setTimeout(animateFireworks, 1000);
+}
+
+animateFireworks();
